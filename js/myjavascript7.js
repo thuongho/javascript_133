d8c9c38766921d1f230e978decdb7564ad3437fd
Thuong Ho <thuong.t.ho@gmail.com>
2014-05-03 23:10:54 -0700

    finish hw7

--- js/myjavascript7.js
+++ js/myjavascript7.js
@@ -7,3 +7,20 @@ function new_window(url)
 	'_new', 'toolbar=0,menubar=0,scrollbars=1,status=0,resizable=1,top=80,left=400px,fullscreen=0,Height=650px,Width=710px', '');
 }
 
+
+// ***** Codes for part 3 *****
+// check password insecure
+
+var redirectMe = function(){
+
+  // get the password from the user
+  var password = document.getElementById('n2').value;
+
+  // test to see if password is correct
+  if (password != "obama"){
+    alert("Incorrect password.");
+  } else {
+    alert("You're in!");
+    window.location.replace("part3_hot.html");
+  }
+};
